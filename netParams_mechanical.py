3122aa72752fd1b368f0e89be421407328993623
lauramedlock <laura.medlock@mail.utoronto.ca>
2021-12-27 14:31:00 -0500

    200 mN, alt tuning, iPV ablation

--- netParams_mechanical.py
+++ netParams_mechanical.py
@@ -85,7 +85,7 @@ netParams.cellParams['ISLETRule'] = cells.INcellRule
 netParams.cellParams['NK1Rule'] = cells.PROcellRule
 
 ## SETTING POPULATION OF SPINAL NEURONS
-netParams.popParams['PKC' ] = {'cellType': 'PKC', 'numCells': 0} # 30 # PKCg+ neurons (excitatory) 200-229 
+netParams.popParams['PKC' ] = {'cellType': 'PKC', 'numCells': 30} # 30 # PKCg+ neurons (excitatory) 200-229 
 netParams.popParams['VGLUT3'] = {'cellType': 'EXdl', 'numCells': 4} # 4 VGLUT3+ neurons (excitatory)  230-233
 netParams.popParams['PV'] = {'cellType': 'IN', 'numCells': 0} # 15 # PV+ neurons (inhibitory)      234-248
 netParams.popParams['DOR' ] = {'cellType': 'EXdl', 'numCells': 30} # 30 # DOR+ neurons (excitatory)  249-278
