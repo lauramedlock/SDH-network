1e16a45e7a5dd37e968689fe0067af8e317c69e2
lauramedlock <laura.medlock@mail.utoronto.ca>
2022-01-02 18:57:31 -0500

    10 mN CD (+25 mV shift all cells)

--- netParams_mechanical.py
+++ netParams_mechanical.py
@@ -105,10 +105,10 @@ netParams.synMechParams['AMPA']  = {'mod': 'AMPA_DynSyn'   , 'tau_rise': 0.1, 't
 netParams.synMechParams['NMDA']  = {'mod': 'NMDA_DynSyn'   , 'tau_rise': 2  , 'tau_decay': 100          }
 netParams.synMechParams['NK13']  = {'mod': 'NK1_DynSyn'    , 'tau_rise': 100, 'tau_decay': 1000         }
 netParams.synMechParams['NK23']  = {'mod': 'NK1_DynSyn'    , 'tau_rise': 200, 'tau_decay': 3000         }
-netParams.synMechParams['GABA']  = {'mod': 'GABAa_DynSyn'  , 'tau_rise': 0.1, 'tau_decay': 20, 'e': -70 }  # GABA for Excitatory Neur
-netParams.synMechParams['GABA2'] = {'mod': 'GABAa_DynSyn'  , 'tau_rise': 0.1, 'tau_decay': 20, 'e': -70 }  # GABA for Inhibitory Neur
-netParams.synMechParams['GLY']   = {'mod': 'Glycine_DynSyn', 'tau_rise': 0.1, 'tau_decay': 10, 'e': -70 }  # Gly for Excitatory Neur
-netParams.synMechParams['GLY2']  = {'mod': 'Glycine_DynSyn', 'tau_rise': 0.1, 'tau_decay': 10, 'e': -70 }  # Gly for Inhibitory Neur
+netParams.synMechParams['GABA']  = {'mod': 'GABAa_DynSyn'  , 'tau_rise': 0.1, 'tau_decay': 20, 'e': -45 }  # GABA for Excitatory Neur
+netParams.synMechParams['GABA2'] = {'mod': 'GABAa_DynSyn'  , 'tau_rise': 0.1, 'tau_decay': 20, 'e': -45 }  # GABA for Inhibitory Neur
+netParams.synMechParams['GLY']   = {'mod': 'Glycine_DynSyn', 'tau_rise': 0.1, 'tau_decay': 10, 'e': -45 }  # Gly for Excitatory Neur
+netParams.synMechParams['GLY2']  = {'mod': 'Glycine_DynSyn', 'tau_rise': 0.1, 'tau_decay': 10, 'e': -45 }  # Gly for Inhibitory Neur
 
 ###################################################################################################################################
 #   Connect the spinal cord stimulation/natural input
@@ -280,7 +280,7 @@ netParams.connParams['Ad_AMPA->DOR'] = {
     'delay': 5.0,
     'loc': 0.50,
     'synMech': 'AMPA'}
-
+i
 netParams.connParams['Ad_NMDA->DOR'] = {
     'oneSynPerNetcon': True,
     'preConds': {'popLabel': 'Ad'}, 
